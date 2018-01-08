cef2f2cf95b424287ec9d2ea93fa794442cf82f4
Igor <igor.tobolyakov@yahoo.com>
2018-01-08 23:16:53 +0200

    test 39

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var app = express();
 
 app.use(express.basicAuth('tobi', 'ferret'));
 app.use(express.logger('dev'));
-app.use(express.static(__dirname + '/repos'));
+app.use(express.static(__dirname + '/views'));
 
 app.listen(3000);
 console.log('Component server listening in port 3000');
