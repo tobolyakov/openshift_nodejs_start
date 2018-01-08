6469df0705b0d71114dc935f06e98f110796059a
Igor <igor.tobolyakov@yahoo.com>
2018-01-08 10:41:17 +0200

    test 11

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+
+  render() {
+    return (
+        <header className="tpl-header">
+            <div className="block-inner container">
+                <div className="name">todos</div>
+            </div>
+      </header>
+    )
+  }
+}
