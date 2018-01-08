bd44953d03abaacccf50b38fed5de129ce7dff32
Igor <igor.tobolyakov@yahoo.com>
2018-01-08 16:17:39 +0200

    test 24

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ var initDb = function(callback) {
   });
 };
 
-app.get('/*', function (req, res) {
+app.get('/', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
   if (!db) {
